3650696aa4f85b4d33d5448d93b5f9785f656035
Comma Device <device@comma.ai>
2026-04-16 14:56:46 +0000

    NMK: ترجمة Reverse Gear

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -784,7 +784,7 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
 
   EventName.reverseGear: {
     ET.PERMANENT: Alert(
-      "Reverse\nGear",
+      "الرجوع\nللخلف",
       "",
       AlertStatus.normal, AlertSize.full,
       Priority.LOWEST, VisualAlert.none, AudibleAlert.none, .2, creation_delay=0.5),
